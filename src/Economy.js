392badec32548d8c1b6ff343429908d41fef556f
Raj Chatterjee <65060180+raj123567@users.noreply.github.com>
2021-04-04 19:14:55 +0530

    Update Economy.js

--- src/Economy.js
+++ src/Economy.js
@@ -1,10 +1,19 @@
 const Discord = require('discord.js')
+const { EventEmitter } = require('events')
+const db = require("quick.db")
 class Economy extends EventEmitter {
     /**
      * @param {Discord.Client} client Discord.js client
      * @param {BotPrefix} prefix Discord Bot Prefix
      */
-    constructor (client, options = {}) {
+    constructor (client, prefix) {
         if (!client) throw new SyntaxError('Invalid Discord client')
         super()
-}{
+
+this.client = client;
+this.prefix = prefix
+this.db = db;
+}
+
+
+}
